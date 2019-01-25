c4af37db6f9a4e4bf8cdbf9ee089ceb0ae81d63a
Kayitesi Jacqueline <kayitesijacqueline@gmail.com>
2019-01-25 09:33:47 +0200

    add files

---
+++ quote.ts
@@ -0,0 +1,10 @@
+export class Quote {
+  
+    public showAuthor:boolean;
+    public showFan:boolean;
+    constructor(public id:number, public name:string, public author:string,public upvotes:number, public downvotes:number, public notedDate:Date){
+      this.showAuthor=false;
+      this.showFan=false;
+    }
+  
+  }
---
+++ quote/quote.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes =[
+    new Quote(1,"My life is my message.","Mahatma Gandhi",0,0,new Date(2017,6,22)),
+    new Quote(2,"Be happy for this moment. This moment is your life.","Omar Khayyam",0,0,new Date(2017,6,22)),
+    new Quote(3,"Get busy living or get busy dying.","Stephen King",0,0,new Date(2017,6,22)),
+        ];
+  toggleDetails(index){
+    this.quotes[index].showAuthor=!this.quotes[index].showAuthor;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
